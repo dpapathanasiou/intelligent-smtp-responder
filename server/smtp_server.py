d2faa58b245e6d7e7076eb1facc7688dd016a18e
Denis Papathanasiou <denis@papathanasiou.org>
2012-03-01 19:21:39 -0500

    Implemented better stream handling, to trap socket errors if they occur during read/write operations

--- server/smtp_server.py
+++ server/smtp_server.py
@@ -2,7 +2,7 @@
 
 """
 
-The custom smtp server: respond to all incoming email ...
+The custom smtp server: respond to all incoming email based on its inbox address
 
 """
 
@@ -21,6 +21,44 @@ idle_threshold = 600.0 # client must complete the interaction within 10 min or b
 
 recipient_domain = domain_logo.lower() # used to check rcpt to values by domain
 
+def printException (ex_args):
+    data = {}
+    try:
+        if isinstance(ex_args, tuple):
+            data[ex_args[0]] = ex_args[1]
+        else:
+            data['state'] = ex_args[0]['state']
+            if ex_args[0]['data']:
+                for k,v in ex_args[0]['data'].items():
+                    if k in ['ip', 'start', 'sender', 'recipients', 'subject']:
+                        data[k] = v
+    except IndexError as i:
+        pass
+    except KeyError as k:
+        pass
+    print 'Exception:', time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()), data
+
+def with_stream (fn, *args):
+    """A higher-order function to attempt stream functions, trapping any socket errors which may occur"""
+    try:
+        return fn(*args)
+    except socket.error, e:
+        if isinstance(e.args, tuple):
+            printException (e.args)
+        else:
+            printException ({"Socket error", e})
+
+def with_stream_write (st, s):
+    """Write the given string (s) to the stream (st)"""
+    with_stream(lambda: st.wfile.write(s))
+
+def with_stream_read (st):
+    """Read a line from the given stream (st)"""
+    data = with_stream(lambda: st.rfile.readline())
+    if data is None:
+        return '' # always return a string, even if there was a socket error
+    else:
+        return data
 
 def client_is_idle (start_time, threshold=idle_threshold):
     """Has this client taken longer than allowed by idle_threshold?"""
@@ -31,7 +69,7 @@ def as_timed_client (cargo, fn):
     stop the client with a bad request reply and close the connection if idle_threshold has been exceeded;
     else proceed with the given function"""
     if client_is_idle(cargo[1]['start']):
-        cargo[0].wfile.write(bad_request+cr_lf)
+        with_stream_write (cargo[0], bad_request+cr_lf)
         return ('done', cargo)
     else:
         return fn()
@@ -40,10 +78,10 @@ def greeting (cargo):
     stream = cargo[0]
     ip = stream.client_address[0]
     if not valid_ip_address(ip):
-        stream.wfile.write(bad_request+cr_lf)
+        with_stream_write (stream, bad_request+cr_lf)
         return ('done', cargo)
     else:
-        stream.wfile.write('220 '+domain_logo+' SMTP'+cr_lf)
+        with_stream_write (stream, '220 '+domain_logo+' SMTP'+cr_lf)
         email_data = cargo[1]
         email_data['ip'] = ip
         email_data['start'] = time.time()
@@ -55,16 +93,16 @@ ehlo_pattern = re.compile('^EHLO', re.IGNORECASE)
 def helo (cargo):
     def _helo ():
         stream = cargo[0]
-        client_msg = stream.rfile.readline()
+        client_msg = with_stream_read (stream)
 
         if smtp_server_debug:
             print 'Debug:', cargo[1]['ip'], time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()), client_msg.rstrip('\r\n')
 
         if helo_pattern.search(client_msg) or ehlo_pattern.search(client_msg):
-            stream.wfile.write('250 Hello'+cr_lf)
+            with_stream_write (stream, '250 Hello'+cr_lf)
             return ('mail', cargo)
         else:
-            stream.wfile.write(bad_request+cr_lf)
+            with_stream_write (stream, bad_request+cr_lf)
             return ('done', cargo)
     return as_timed_client(cargo, _helo)
 
@@ -73,7 +111,7 @@ mail_pattern = re.compile('^MAIL', re.IGNORECASE)
 def mail (cargo):
     def _mail ():
         stream = cargo[0]
-        client_msg = stream.rfile.readline()
+        client_msg = with_stream_read (stream)
 
         if smtp_server_debug:
             print 'Debug:', cargo[1]['ip'], time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()), client_msg.rstrip('\r\n')
@@ -81,15 +119,15 @@ def mail (cargo):
         if mail_pattern.search(client_msg):
             sender = get_email_address(client_msg)
             if sender is None or not valid_email_address(sender):
-                stream.wfile.write(bad_request+cr_lf)
+                with_stream_write (stream, bad_request+cr_lf)
                 return ('done', cargo)
             else:
-                stream.wfile.write('250 Ok'+cr_lf)
+                with_stream_write (stream, '250 Ok'+cr_lf)
                 email_data = cargo[1]
                 email_data['sender'] = sender
                 return ('rcpt', (stream, email_data))
         else:
-            stream.wfile.write(bad_request+cr_lf)
+            with_stream_write (stream, bad_request+cr_lf)
             return ('done', cargo)
     return as_timed_client(cargo, _mail)
 
@@ -104,7 +142,7 @@ def rcpt (cargo):
     client_error = False
 
     while 1:
-        client_msg = stream.rfile.readline()
+        client_msg = with_stream_read (stream)
 
         if smtp_server_debug:
             print 'Debug:', cargo[1]['ip'], time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()), client_msg.rstrip('\r\n')
@@ -120,7 +158,7 @@ def rcpt (cargo):
             else:
                 if recipient not in recipients:
                     recipients.append(recipient)
-                stream.wfile.write('250 Ok'+cr_lf)
+                with_stream_write (stream, '250 Ok'+cr_lf)
         elif data_pattern.search(client_msg):
             break
         else:
@@ -138,7 +176,7 @@ def rcpt (cargo):
     if client_error or len(server_recipients) == 0:
         # this email was not sent to an inbox that we can respond to
         # so stop the client with a bad request reply and close the connection 
-        stream.wfile.write(bad_request+cr_lf)
+        with_stream_write (stream, bad_request+cr_lf)
         return ('done', cargo)
     else:
         email_data['inbox'] = inbox
@@ -152,14 +190,14 @@ def data (cargo):
     stream = cargo[0]
     email_data = cargo[1]
 
-    stream.wfile.write('354 End data with \\r\\n.\\r\\n'+cr_lf)
+    with_stream_write (stream, '354 End data with \\r\\n.\\r\\n'+cr_lf)
 
     contents = []
     client_error = False
     subject = None
 
     while 1:
-        client_msg = stream.rfile.readline()
+        client_msg = with_stream_read (stream)
         if client_is_idle(email_data['start']):
             client_error = True
             break
@@ -179,10 +217,10 @@ def data (cargo):
                         pass
 
     if client_error or len(contents) == 0:
-        stream.wfile.write(bad_request+cr_lf)
+        with_stream_write (stream, bad_request+cr_lf)
         return ('done', cargo)
     else:
-        stream.wfile.write('250 Ok: queued'+cr_lf)
+        with_stream_write (stream, '250 Ok: queued'+cr_lf)
         email_data['contents'] = ''.join(contents)
         return ('process', (stream, email_data))
 
@@ -191,23 +229,6 @@ def process (cargo):
     process_email(cargo[1]) 
     return ('done', cargo)
 
-
-def printException (ex_args):
-    data = {}
-    try:
-        if isinstance(ex_args, tuple):
-            data[ex_args[0]] = ex_args[1]
-        else:
-            data['state'] = ex_args[0]['state']
-            if ex_args[0]['data']:
-                for k,v in ex_args[0]['data'].items():
-                    if k in ['ip', 'start', 'sender', 'recipients', 'subject']:
-                        data[k] = v
-    except IndexError as i:
-        pass
-    except KeyError as k:
-        pass
-    print 'Exception:', time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()), data
 
 class SMTPRequestHandler (SocketServer.StreamRequestHandler):
     def handle (self):
