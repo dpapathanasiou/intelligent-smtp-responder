43ab086e2b6fdea596f7c9ecb61e74d9e0bdcd24
Denis Papathanasiou <denis@papathanasiou.org>
2013-12-26 10:17:53 -0500

    Updated the run script to notify on failure every 10 restarts, since a systemic error would flood the recipient inbox with messages

--- run_smtps.sh
+++ run_smtps.sh
@@ -6,11 +6,20 @@
 
 smtps=`pwd`
 
+counter=0
 while true
 do
     logfile=`date | sed -e 's/ /_/g' | sed -e 's/:/_/g'`
     su -c "export PYTHONPATH=$PYTHONPATH:$smtps:$smtps/server:$smtps/utils:$smtps/agents; python -c 'import smtp_server; smtp_server.start()' > /tmp/smtp_server_$logfile.log 2>&1" daemon
-    # error: send email and restart    
-    mail -s "SMTP server error!" support@example.org < /tmp/smtp_server_$logfile.log
+
+    # error: send email (only once every 10 restarts) and attempt to restart
+    notifycheck=`expr $counter % 10`
+    if [ $notifycheck -eq 0 ]
+    then
+        mail -s "SMTP server error! $counter restarts attempted" support@example.org < /tmp/smtp_server_$logfile.log
+    fi
+
+    counter=`expr $counter + 1`
+    sleep 10
 done
 
